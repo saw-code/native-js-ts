3b239f9ada1002bdb92f4b0d34aae34b5737ce89
saw-code <saw.coding@gmail.com>
2023-01-03 17:46:36 +0300

    introduction to native JS and TS

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {value01_01} from "./01/01";
+
+let a = value01_01
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
