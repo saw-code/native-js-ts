4613bde05a40114347715de5756ee6539927880c
saw-code <saw.coding@gmail.com>
2023-01-04 03:15:07 +0300

    Lesson 1 - testing

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {value01_01} from "./01/01";
+import {splitIntoWords} from "./01-hello-tests/01";
 
-let a = value01_01
+const sentense = "Hello my friends!"
+const result = splitIntoWords(sentense)
+console.log(result)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
